57e78429bec9cbc10e7e2e016d908fc839a19cb2
Joseph Hart <josephrhart@gmail.com>
2020-03-19 02:06:44 -0500

    feat: adding Ndash.Join

---
+++ NdashLib/Join.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NdashLib
+{
+    public static partial class Ndash
+    {
+        public static string Join<T>(this IEnumerable<T> collection, string separator = ",")
+        {
+            return string.Join(separator, collection);
+        }
+    }
+}
